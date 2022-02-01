4356ab2dd6fbe16da9a4d09d379cdc7037472647
ezecor <ezcorreia@gmail.com>
2022-02-01 17:34:40 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,13 +1,18 @@
 var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
 });
+var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+    maxZoom: 20
+});
 var map = L.map('map', {
     center: [40.85, -8.41],
     minZoom: 9,
-    maxZoom: 9,
+    maxZoom: 12,
     //zoom: 8,
     pan: false,
-    zoomControl: false,
+    //zoomControl: false,
     layers: [CartoDB_Positron]
 });
 var dist = L.geoJSON(distrito, {
